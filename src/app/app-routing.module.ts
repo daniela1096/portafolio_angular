33e676d7c0b391d75ce9287fddee6223a269d6aa
Daniela Diaz <dani.diaz1096@gmail.com>
2019-01-31 13:04:15 -0500

    Fin sección 3

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+//MODULO ENCARGADO DE LA ESPECIFICACIÓN DE LAS RUTAS
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'; //El routerModule le dice a angular si son las rutas principales o las rutas hijas
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+ 
+const app_routes: Routes = [
+    { path : '', component: PortafolioComponent },
+    { path : 'about', component: AboutComponent },
+    { path : 'item', component: ItemComponent },
+    { path : '**', pathMatch:'full', redirectTo: '' }
+]
+
+
+@NgModule({
+    imports:[
+        RouterModule.forRoot(app_routes, { useHash:true })
+    ],
+    exports:[
+        RouterModule
+    ]
+
+})
+
+export class appRoutingModule{
+
+}
