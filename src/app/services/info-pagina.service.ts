cc2e8e4c0ca92235765f3e3cb5e0bad465c42dce
Daniela Diaz <dani.diaz1096@gmail.com>
2019-08-02 11:17:30 -0500

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,30 +1,53 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; //Aqui se encuentra el servicio httpClient
-import { InfoPagina } from '../interfaces/info-pagina.interfaces';
+import { InfoPagina} from '../interfaces/info-pagina.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
-  
+
+  // Llamo a las interfaces y las asigno a una variable
   info: InfoPagina = {};
-  //info: any = {};
+  //equipo: EquipoPagina = {};
   cargada = false; // me va a indicar cuando tenga cargada la información
+  //info: any = {};
+  //equipo: InfoEquipo = {};
+  equipo: any[] = [];
 
 
   constructor(private http: HttpClient) { //con este servicio puedo realizar peticiones a cualquier servidor
+    //console.log('Info pagina cargada');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
-    console.log('Info pagina cargada');
-
+  private cargarInfo() {
     //Leer el archivo json
-
+    
     this.http.get('assets/data/data-pagina.json')
-    .subscribe( (resp: InfoPagina) =>{
-      //console.log(resp)
-      //console.log(resp['facebook']);
-      this.cargada = true;
-      this.info = resp;
-      console.log(resp)
-    });
+      .subscribe(( resp: InfoPagina) => {
+        //console.log(resp)
+        //console.log(resp['facebook']);
+        this.cargada = true;
+        this.info = resp;
+        console.log(resp)
+      });
+
+  }
+
+
+  private cargarEquipo() {
+    //Leer el archivo json
+    this.http.get('https://angular-html-d9ca1.firebaseio.com/equipo.json')
+      .subscribe((resp: any) => {
+        //console.log(resp)
+        //console.log(resp['facebook']);
+        this.cargada = true;
+        this.equipo = resp;
+        console.log(resp)
+      });
+
+
   }
 }
